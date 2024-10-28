74e5ab0f799aa48db4a7ed817ef441d5073dca71
guisiltro <167353893+guisiltro@users.noreply.github.com>
2024-10-28 08:46:03 -0300

    aaaaaaaa
    aaaaaaaaa

---
+++ script.js
@@ -0,0 +1,56 @@
+//QUESTÃO 1
+//let produtos = ["Celular", "Mouse", "Monitor", "Notebook", "Impressora", "Teclado", "Televisão", "Fone", "Webcam", "HD Externo"];
+//produtos [0] = celular
+//produtos [3] = notebook
+//produtos [6] =  televisão
+//QUESTÃO 2
+//let numeros = [2, 5, 8, 13, 16, 21];
+//let corretoA = []
+//let aleatorio - Math.random(*10)
+//numeros.forEach(function(corretoA){
+
+
+
+  //  if(resultado === 0){
+        
+      //  correto.push(corretoA)
+
+   // }
+  //  else{
+     //   let numero = corretoA*aleatorio
+     //   corretoA.push(numero)
+        
+    //}
+   // console.log(corretoA)
+//})
+
+//QUESTÃO 3
+//let numeros = [1.2, 3.7, 4.5, 6.1, 8.9];
+//let correto = []
+
+//numeros.forEach(function(arrumar){
+    //correto = Math.ceil(arrumar)
+   // console.log(correto)
+
+//})
+
+//QUESTÃO 4
+//let nome = prompt("Seu nome é?")
+//let LETRAS = nome.split("")
+//let contador = 0;
+//LETRAS.forEach(function(QUANTIDADE){
+   // if(QUANTIDADE != " ")
+    //    contador++
+//})
+
+//console.log(contador++)
+
+//QUESTÃO 5
+let original = [4, 5, 6];
+ 
+let numerosnovos = [1, 2, 3];
+ 
+numerosnovos.forEach(function(original){
+    original.push(numerosnovos + original)})
+   
+    console.log(numerosnovos + original)
